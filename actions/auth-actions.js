a933f94e6a1cf3ebed93caa04ed0b958e397731f
Logan Barsell <loganjbars@gmail.com>
2025-01-29 18:34:07 -0800

    create auth inst. + session cookie + verify auth + login/logout

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,10 +1,11 @@
 'use server';
 
-import { hashUserPassword } from '@/lib/hash';
-import { createUser } from '@/lib/user';
+import { createAuthSession, destroySession } from '@/lib/auth';
+import { hashUserPassword, verifyPassword } from '@/lib/hash';
+import { createUser, getUserByEmail } from '@/lib/user';
 import { redirect } from 'next/navigation';
 
-export async function signup(formData) {
+export async function signup(prevState, formData) {
   const email = formData.get('email');
   const password = formData.get('password');
 
@@ -29,7 +30,9 @@ export async function signup(formData) {
 
   try {
     // store in db (create a user)
-    createUser(email, hashedPassword);
+    const id = createUser(email, hashedPassword);
+    await createAuthSession(id);
+    redirect('/training');
   } catch (error) {
     if (error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
       return {
@@ -40,6 +43,47 @@ export async function signup(formData) {
     }
     throw error;
   }
+}
 
+export async function login(prevState, formData) {
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  // check if user exists with this email
+  const existingUser = getUserByEmail(email);
+  if (!existingUser) {
+    return {
+      errors: {
+        user: 'Could not authenticate user, please check your credentials',
+      },
+    };
+  }
+
+  // check if passwords match
+  const passMatch = verifyPassword(existingUser.password, password);
+  if (!passMatch) {
+    return {
+      errors: {
+        user: 'Could not authenticate user, please check your credentials',
+      },
+    };
+  }
+
+  // create authentication session and redirect
+  await createAuthSession(existingUser.id);
   redirect('/training');
+}
+
+export async function auth(mode, prevState, formData) {
+  console.log(mode, prevState, formData);
+  if (mode === 'login') {
+    return login(prevState, formData);
+  }
+  return signup(prevState, formData);
+}
+
+export async function logout() {
+  console.log('LOGOUT');
+  await destroySession();
+  redirect('/');
 }
--- app/(auth)/training/page.js
+++ app/(auth)/training/page.js
@@ -1,15 +1,26 @@
+import { verifyAuth } from '@/lib/auth';
 import { getTrainings } from '@/lib/training';
+import { redirect } from 'next/navigation';
 
 export default async function TrainingPage() {
+  const loggedIn = await verifyAuth();
+  console.log(loggedIn);
+  if (!loggedIn.user) {
+    return redirect('/');
+  }
+
   const trainingSessions = getTrainings();
 
   return (
     <main>
       <h1>Find your favorite activity</h1>
-      <ul id="training-sessions">
-        {trainingSessions.map((training) => (
+      <ul id='training-sessions'>
+        {trainingSessions.map(training => (
           <li key={training.id}>
-            <img src={`/trainings/${training.image}`} alt={training.title} />
+            <img
+              src={`/trainings/${training.image}`}
+              alt={training.title}
+            />
             <div>
               <h2>{training.title}</h2>
               <p>{training.description}</p>
